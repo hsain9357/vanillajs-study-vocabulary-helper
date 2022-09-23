7b4b00521c5d33597cb0685d816748a968efa47d
hsain9357 <hsain9357@gmail.com>
2022-09-23 19:39:53 +0300

    shuffled quiz's elements

--- js/quiz.js
+++ js/quiz.js
@@ -1,49 +1,69 @@
 const row1 = document.querySelector(".row1 ul");
 const row2 = document.querySelector(".row2 ul");
-class demo {
-  constructor() {
-    this.intialize();
-  }
-  intialize() {
-    try {
-      this.numberOfWordsInLocalStorage = localStorage.getItem("number");
-      this.currentPrograss = 0;
-      this.currentNumberFetched = 0;
-      this.startFetchNumber = 1;
-      this.endFetchNumber = 11;
-      if (this.numberOfWordsInLocalStorage < 10) {
-        this.fetchData(1, this.numberOfWordsInLocalStorage);
-      } else {
-        this.fetchData(this.startFetchNumber, this.endFetchNumber);
-      }
-    } catch (e) {
-      alert(e);
+class Quiz {
+
+  constructor() { this.demo(); }
+  demo() {
+
+    this.numberOfWordsInLocalStorage = localStorage.getItem("number");
+    this.currentPrograss = 0;
+    this.currentNumberFetched = 0;
+    this.startFetchNumber = 1;
+    this.endFetchNumber = 11;
+
+    if (this.numberOfWordsInLocalStorage < 10) {
+      this.fetchData(1, this.numberOfWordsInLocalStorage);
+    } else {
+      this.fetchData(this.startFetchNumber, this.endFetchNumber);
     }
   }
+
   fetchData(Start, End) {
-    if (this.numberOfWordsInLocalStorage == 0) return;
+    if (this.numberOfWordsInLocalStorage == 0)
+      return;
     this.removePrevousLists();
+      let arrayOfDefinitions = [];
+      let arrayOfWords = [];
     for (let i = Start; i <= End; i++) {
       const mainInfo = JSON.parse(localStorage.getItem(`word.${i}`));
+
       if (mainInfo && mainInfo.mainSentence && mainInfo.mainworld) {
-        this.appendLiElment({
-          attributeNumber: i,
-          text: mainInfo.mainworld,
-          row: "row1",
-        });
-        this.appendLiElment({
-          attributeNumber: i,
-          text: mainInfo.mainSentence,
-          row: "row2",
-        });
+          arrayOfWords.push({value:mainInfo.mainworld, currentNumbAttr:i})
+          arrayOfDefinitions.push({value:mainInfo.mainSentence, currentNumbAttr:i})
+
       } else {
         this.endFetchNumber -= 1;
       }
     }
+      if(arrayOfWords.length){
+          arrayOfWords = this.shuffle(arrayOfWords)
+          arrayOfDefinitions = this.shuffle(arrayOfDefinitions)
+
+          for (let i = 0; i < arrayOfWords.length; i++) {
+              const word = arrayOfWords[i];
+              const  definition = arrayOfDefinitions[i];
+
+              this.appendLiElment({
+                  attributeNumber : word.currentNumbAttr,
+                  text : word.value,
+                  row : "row1",
+              });
+
+              this.appendLiElment({
+                  attributeNumber : definition.currentNumbAttr,
+                  text : definition.value,
+                  row : "row2",
+              });
+
+          }
+      }
+
     this.setEvents();
   }
-  appendLiElment({ attributeNumber, text, row }) {
+  appendLiElment({attributeNumber = 0, text='', row=''}) {
+
     const Li = document.createElement("li");
+
     Li.setAttribute("number", attributeNumber.toString());
     Li.innerText = text;
     if (row == "row1") {
@@ -57,18 +77,23 @@ class demo {
     let ListsRow2 = document.querySelectorAll(".row2 ul li");
     let clickedPreviously1;
     let clickedPreviously2;
+
     ListsRow1.forEach((element) => {
       element.addEventListener("click", () => {
         if (clickedPreviously1) {
+
           clickedPreviously1.classList.remove("choosed");
           clickedPreviously1 = element;
           clickedPreviously1.classList.add("choosed");
           this.checkWin(clickedPreviously1, clickedPreviously2, "row1");
+
           if (clickedPreviously2 && clickedPreviously1) {
             clickedPreviously1 = null;
             clickedPreviously2 = null;
           }
+
         } else {
+
           clickedPreviously1 = element;
           clickedPreviously1.classList.add("choosed");
           this.checkWin(clickedPreviously1, clickedPreviously2, "row1");
@@ -105,7 +130,8 @@ class demo {
   checkWin(Li1, Li2, lastOnChoosed) {
     let LiChoosed1 = Li1.getAttribute("number");
     let LiChoosed2 = Li2.getAttribute("number");
-    if (!LiChoosed2 && !LiChoosed2) return;
+    if (!LiChoosed2 && !LiChoosed2)
+      return;
     if (LiChoosed1 == LiChoosed2) {
       Li1.classList.add("correct");
       Li2.classList.add("correct");
@@ -122,9 +148,8 @@ class demo {
       if (lastOnChoosed === "row1") {
         Li1.classList.add("wrong");
         const correctNum = Li2.getAttribute("number");
-        const correctLi = document.querySelector(
-          `.row1 ul li[number="${correctNum}"]`
-        );
+        const correctLi =
+            document.querySelector(`.row1 ul li[number="${correctNum}"]`);
         correctLi.classList.add("correct");
         setTimeout(() => {
           Li1.classList.remove("choosed");
@@ -136,9 +161,8 @@ class demo {
       if (lastOnChoosed == "row2") {
         Li2.classList.add("wrong");
         const correctNum = Li1.getAttribute("number");
-        const correctLi = document.querySelector(
-          `.row2 ul li[number="${correctNum}"]`
-        );
+        const correctLi =
+            document.querySelector(`.row2 ul li[number="${correctNum}"]`);
         correctLi.classList.add("correct");
         setTimeout(() => {
           correctLi.classList.remove("correct");
@@ -160,16 +184,25 @@ class demo {
       }
     }
   }
-  removeElement(element) {
-    element.remove();
-  }
+  removeElement(element) { element.remove(); }
   removePrevousLists() {
-    document
-      .querySelectorAll(".row1 li")
-      .forEach((element) => element.remove());
-    document
-      .querySelectorAll(".row2 li")
-      .forEach((element) => element.remove());
+    document.querySelectorAll(".row1 li")
+        .forEach((element) => element.remove());
+    document.querySelectorAll(".row2 li")
+        .forEach((element) => element.remove());
   }
+
+shuffle(array) {
+  var tmp, current, top = array.length;
+  if(top) while(--top) {
+
+
+    current = Math.floor(Math.random() * (top + 1));
+    tmp = array[current];
+    array[current] = array[top];
+    array[top] = tmp;
+  }
+  return array;
 }
-const main = new demo();
+}
+const main = new Quiz();
